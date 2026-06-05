0282f06c4305cfb7ece6348135b8187d04de6f33
Nensi Gohel <nensigohel25@gmail.com>
2026-06-05 17:26:37 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//#define pi 3.14
+
+int main(){
+    float principal, rateofinterce, time;
+
+
+    printf("enter value of principal:");
+    scanf("%f", &principal);
+
+    printf("enter value of rateofinterce:"); 
+    scanf("%f", &rateofinterce);
+
+    printf("area of triangle is %f\n" );
+
+}
