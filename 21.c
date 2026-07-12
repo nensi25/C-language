30efccde311a49d06233046b9ce79e5ea16ddb83
Nensi Gohel <nensigohel25@gmail.com>
2026-07-12 16:34:43 +0530

    Add files via upload

---
+++ 21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int no;//no=number
+
+
+printf("enter no:");
+scanf("%d", &no);
+
+
+if(no%2==0)
+
+{
+    printf("is even");
+}
+ else
+{
+    printf("is odd");
+}
+}
