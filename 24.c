c97f18c31cd09d11b64c36949c94292d0507884c
Nensi Gohel <nensigohel25@gmail.com>
2026-07-18 20:23:07 +0530

    Add files via upload

---
+++ 24.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+
+char ch;
+
+print("enter char:");
+scanf("%c", &ch);
+
+if(ch >='A' && ch >='z');
+
+
+{
+    printf("capital");
+}
+
+else 
+if{(ch >='a' && ch>='z');
+
+    printf("small");
+}
+
+else if(ch >=0 && int >='9');
+{
+    printf("digit");
+}
+
+else if(ch >=);
+{
+    printf("special character");
+}
+
+
+}
