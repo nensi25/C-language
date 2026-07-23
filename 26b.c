afbe65d8de8c10a33a92598743f4315c30a9d702
Nensi Gohel <nensigohel25@gmail.com>
2026-07-23 21:00:33 +0530

    Add files via upload

---
+++ 26b.c
@@ -0,0 +1,19 @@
+//switch case
+
+#include<stdio.h>
+int main(){
+int m1, m2, m3, result, per;
+
+printf("enter marks of three subject:");
+scanf("%d%d%d", &m1, &m2, &m3);
+per = (m1+m2+m3)/3;
+per=per/10;
+
+
+switch(per)
+
+{
+    case10; case9; case8;
+}
+
+
