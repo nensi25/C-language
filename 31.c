a0769402f1ebb4740940dd74e0ee554b3c1d11b2
Nensi Gohel <nensigohel25@gmail.com>
2026-08-29 18:29:58 +0530

    Add files via upload

---
+++ 31.c
@@ -0,0 +1,23 @@
+//even or odd
+#include <stdio.h>
+int main(){
+
+int no, lastdigit;
+scanf("%d", &no);
+lastdigit = no%10;
+if (lastdigit %2==0)
+{printf("even");
+}
+else 
+{printf("odd");
+}
+}
+
+
+
+
+
+
+
+
+
